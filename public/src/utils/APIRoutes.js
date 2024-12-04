a1d5d980df4a4914ecd03ae179ccb2e2bff683b3
lateef taiwo <66183213+lateef-taiwo@users.noreply.github.com>
2024-12-04 09:24:08 -0600

    Update APIRoutes.js

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "localhost:5000";
+export const host = "http://52.200.226.179:5000";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
